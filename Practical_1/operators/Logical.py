b4089c905d433bf788814e4270809906b45c3199
Chimirinzin11 <juniordracula148@gmail.com>
2025-11-14 11:43:18 +0600

    Delete Practical_1 directory

--- Practical_1/operators/Logical.py
+++
@@ -1,8 +0,0 @@
-#Exercise
-#1
-x, y = True, False
-print(f"x={x}, y={y}")
-print(f"x and y: {x and y}")  # Logical AND
-print(f"x or y: {x or y}")    # Logical OR
-print(f"not x: {not x}")      # Logical NOT 
-print(f"not y: {not y}")      # Logical NOT on y
