f3d5250c08e17a6d7a0ed5bf503a50c17beada8a
Chimirinzin11 <juniordracula148@gmail.com>
2025-11-14 12:04:11 +0600

    Delete Practical_7 directory

--- Practical_7/Duplicates.py
+++
@@ -1,9 +0,0 @@
-def containsDuplicate(x):
-    seen = set()
-    for num in x:
-        if num in seen:
-            return True
-        seen.add(num)
-    return False
-x=[1,2,3,4,5,6,7,7]
-print(containsDuplicate(x))
